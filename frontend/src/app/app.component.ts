50a628726b8ef5306b9440a7eba897d3891cf8a5
Denin <denersonh@gmail.com>
2019-09-15 18:37:41 -0300

    [master] - Finalização marcação no mapa e seleção de Rotas.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,23 +10,28 @@ import PlaceResult = google.maps.places.PlaceResult;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Rotas e Pedágios';
   lat: any;
   lng: any;
   map: any;
   zoom = 10;
   infoWindow: any;
   waypoints: any;
-  public appearance = Appearance;
-  public selectedAddress: PlaceResult;
+  waypointsIcon = {url: 'assets/images/estrada-com-pedagio.svg', scaledSize: {//ícone de pedágio.
+    width: 20,
+    height: 20
+  }}
+  appearance = Appearance;
+  selectedAddress: PlaceResult;
+  origin: any;//Origem para componente de Direction.
+  destination: any;//Destino para componente de Direction
 
   constructor(public httpClient: HttpClient) {}
 
   ngOnInit() {
-    this.loadWaypoints();
-    
+    this.loadWaypoints();//Buscar waypoints(Pedágios ao iniciar aplicação.)    
   }
-  //Get My Location
+  //Definir localização inicial da aplicação ao ser iniciada.
   myLocation() {
     if (navigator.geolocation) {
       navigator.geolocation.watchPosition((pos: Position) => {
@@ -37,7 +42,7 @@ export class AppComponent {
       this.handleLocationError(false, this.infoWindow, this.map.getCenter());
     }
   }
-
+  //Retornar erro caso ocorra problemas na busca de localizações.
   handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
@@ -46,22 +51,31 @@ export class AppComponent {
     infoWindow.open(this.map);
   }
   //Get Pedágios localizações
-  loadWaypoints(){
+  loadWaypoints(){//Buscar informações de pedágios do JSON criado com informações da planilha.
     this.httpClient.get('assets/data/waypoints.json')
     .subscribe(results => {
       this.waypoints = results;
-      this.myLocation();
+      this.myLocation();//Iniciar na localização do Usuário ao iniciar aplicação.
     })
   }
   //Autocomplete Input
+  //Exibir resultados com base nas informações inseridas nos inputs de origem e destino.
   onAutocompleteSelected(result: PlaceResult) {
     console.log('onAutocompleteSelected: ', result);
   }
- 
-  onLocationSelected(location: Location) {
+  //Definir ponto de origem Selecionado.
+  onLocationSelectedOrigin(location: Location) {
     console.log('onLocationSelected: ', location);
     this.lat = location.latitude;
     this.lng = location.longitude;
-    this.zoom = 15;
+    this.zoom = 15;//Definir zoom do maps exibido.
+    this.origin = { lat: this.lat, lng: this.lng };
+  }
+  //Definir ponto de Destino selecionado.
+  onLocationSelectedDestination(location: Location) {
+    console.log('onLocationSelected: ', location);
+    this.lat = location.latitude;
+    this.lng = location.longitude;
+    this.destination = { lat: this.lat, lng: this.lng };
   }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+//Modulos para utilizar google Maps no Angular
 import { AgmCoreModule } from '@agm/core';
+import { AgmDirectionModule } from 'agm-direction';
 import { MatGoogleMapsAutocompleteModule } from '@angular-material-extensions/google-maps-autocomplete';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -14,13 +18,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     HttpClientModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBxPygC3tBioOdXGfLnj9Q6gT-QGKGLXhk&libraries',
       libraries: ['places']
     }),
-    MatGoogleMapsAutocompleteModule.forRoot()
+    MatGoogleMapsAutocompleteModule.forRoot(),
+    AgmDirectionModule,//Módulo pacote agmr Rotas
+    MaterialModule//Módulo Componentes Angular Material
   ],
   providers: [],
   bootstrap: [AppComponent]
